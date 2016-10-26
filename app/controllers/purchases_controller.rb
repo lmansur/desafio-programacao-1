93880007f639d0406422eccfc58886e72e02524b
Lucas Mansur <lucas.mansur2@gmail.com>
2016-10-25 23:22:13 -0200

    create import action

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,4 +5,13 @@ class PurchasesController < ApplicationController
     def new
       @purchase = Purchase.new
     end
+
+    def import
+      csv_handler = CsvHandler.new(params[:file])
+      csv_handler.renameHeaders
+      csv_handler.import
+      revenue = csv_handler.totalRevenue
+      flash[:revenue] = "Total revenue from imported file: #{revenue}"
+      redirect_to new_purchase_path
+    end
 end
